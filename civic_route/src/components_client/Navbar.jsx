9977c42cb0d6fcb8638fed29cd04f59d3d173c9d
Anasbabu Meman <anaslambadiya@gmail.com>
2025-04-12 16:40:31 +0530

    a

--- civic_route/src/components_client/Navbar.jsx
+++ civic_route/src/components_client/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import logo from "./logo.png"; // Make sure this path is correct
-import "./Nav.css";
+import "./nav.css";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
